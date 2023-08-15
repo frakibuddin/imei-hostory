9a307ff3b81db6da6e96bf41407876e96c2cc0a2
frakibuddin <frakibuddin360@gmail.com>
2023-08-16 01:10:09 +0600

    update

--- userpanel/js/script.js
+++ userpanel/js/script.js
@@ -4,3 +4,18 @@ let sidebar = document.querySelector(".sidebar");
 menu_btn.onclick = () => {
   sidebar.classList.toggle("active");
 };
+
+// popup
+let popupShowBtn = document.querySelectorAll(".popup-show");
+let popupBox = document.querySelector(".popup");
+let popupCloseBnt = document.querySelector(".popup-header span");
+
+popupShowBtn.forEach((element) => {
+  element.onclick = () => {
+    popupBox.classList.add("active");
+  };
+});
+
+popupCloseBnt.onclick = () => {
+  popupBox.classList.remove("active");
+};
